d3fbf5bda4eb380edbe47fe794ac89aff2183649
rasa7aj <shahin.xt7aj@gmail.com>
2016-05-22 00:59:44 +0430

    Update echos.lua

--- plugins/echos.lua
+++ plugins/echos.lua
@@ -13,8 +13,7 @@
 
 do
     local function run(msg, matches)
-        text = 'باعشق مدیر😍 چیکار داری 
-سارا عشق مدیر هست 👈'..lang_text(msg.to.id, 'commandsT')..':\n'
+        text = 'باعشق مدیر😍 چیکار داری سارا عشق مدیر هست 👈'..lang_text(msg.to.id, 'commandsT')..':\n'
         local space = '\n'
         if matches[1] == '❤' and not matches[2] then
             if permissions(msg.from.id, msg.to.id, "mod_commands") then
