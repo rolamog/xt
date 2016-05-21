debde2b8f569875e578e6ab5dd2f914853daa778
rasa7aj <shahin.xt7aj@gmail.com>
2016-05-21 23:47:19 +0430

    Update echos.lua

--- plugins/echos.lua
+++ plugins/echos.lua
@@ -1,19 +1,72 @@
-local function run(msg, matches)
-  local text = matches[1]
-  local b = 1
+--------------------------------------------------
+--      ____  ____ _____                        --
+--     |    \|  _ )_   _|___ ____   __  __      --
+--     | |_  )  _ \ | |/ ·__|  _ \_|  \/  |     --
+--     |____/|____/ |_|\____/\_____|_/\/\_|     --
+--                                              --
+--------------------------------------------------
+--                                              --
+--       Developers: @Josepdal & @MaSkAoS       --
+--     Support: @Skneos,  @iicc1 & @serx666     --
+--                                              --
+--------------------------------------------------
 
-  while b ~= 0 do
-    text = text:trim()
-    text,b = text:gsub('^!+','')
-  end
-  return text
+do
+    local function run(msg, matches)
+        text = 'عشق مدیر'..lang_text(msg.to.id, 'commandsT')..':\n'
+        local space = '\n'
+        if matches[1] == 'commands' and not matches[2] then
+            if permissions(msg.from.id, msg.to.id, "mod_commands") then
+                local langHash = 'langset:'..msg.to.id
+                local lang = redis:get(langHash)
+                for v,plugin in pairs(_config.enabled_plugins) do
+                    local textHash = 'lang:'..lang..':'..plugin..':0'
+                    if redis:get(textHash) then
+                        for i=1, tonumber(lang_text(msg.to.id, plugin..':0')), 1 do
+                            text = text..lang_text(msg.to.id, plugin..':'..i)..'\n'
+                        end
+                        text = text..space
+                    end
+                end
+            else
+                text = text..lang_text(msg.to.id, 'moderation:5')..'\n'
+                text = text..lang_text(msg.to.id, 'version:1')..'\n'
+                text = text..lang_text(msg.to.id, 'rules:1')..'\n'
+            end
+        elseif matches[1] == 'commands' and matches[2] then
+            if permissions(msg.from.id, msg.to.id, "mod_commands") then
+                local langHash = 'langset:'..msg.to.id
+                local lang = redis:get(langHash)
+                for v,plugin in pairs(_config.enabled_plugins) do
+                    if plugin == matches[2] then
+                        local textHash = 'lang:'..lang..':'..plugin..':0'
+                        if redis:get(textHash) then
+                            for i=1, tonumber(lang_text(msg.to.id, plugin..':0')), 1 do
+                                text = text..lang_text(msg.to.id, plugin..':'..i)..'\n'
+                            end
+                        end
+                        return text
+                    end
+                end
+                return 'ℹ️ '..lang_text(msg.to.id, 'errorNoPlug')
+            else
+                return '🚫 '..lang_text(msg.to.id, 'require_mod')
+            end
+        end
+        return text
+    end
+
+    return {
+        patterns = {
+            "^(سارا)$",
+            "^(commands) (.+)"
+        }, 
+        run = run 
+    }
 end
 
-return {
-  description = "Simplest plugin ever!",
-  usage = "!echo [با عشق شاهین چی کار داری]: echoes the msg",
-  patterns = {
-    "^[!/]Echo +(.+)$"
-  }, 
-  run = run 
-}
+for v,user in pairs(_gbans.gbans_users) do
+    if tonumber(user) == tonumber(user_id) then
+        return true
+    end
+  end
